c105219802ff61e3e6f61210c1c61422174e6565
unknown <you@example.com>
2020-11-25 17:48:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+
+function abc() {
+
+    var nationality = document.getElementById("nationalty").Value;
+    var age = document.getElementById("age").value;
+    var education = document.getElementById("education").value;
+
+    if (nationality === "pakistan" && age > 18 && age < 25  education === graduation ){
+
+        document.getElementById("result").innerHTML=
+
+
+    }
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
