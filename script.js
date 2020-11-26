7c580e53f855edad412ad2223db282e9d5e35bdb
unknown <you@example.com>
2020-11-26 16:50:13 +0500

    first commit

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 
-function abc() {
+function abc(){
 
-    var nationality = document.getElementById("nationalty").Value;
+    var nationality = document.getElementById("nationality").value;
     var age = document.getElementById("age").value;
     var education = document.getElementById("education").value;
 
-    if (nationality === "pakistan" && age > 18 && age < 25  education === graduation ){
 
-        document.getElementById("result").innerHTML=
+       console.log(nationality , education)
+
+       age=parseInt(age);
+     
+
+ if (nationality === "pakistan" && (age >= 18 && age <= 25) || education === "graduation" )
+ {
+        document.getElementById("result").innerHTML="<h1>eligible</h1>";
 
 
     }
 
+   else{
+
+
+        document.getElementById("result").innerHTML="<h1>not eligible</h1>"
+    
+
+
+   } 
 
 
   
